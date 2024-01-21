6d188e236064a3384aaa114efab6119567997d1a
adihehe <157320585+adihehe@users.noreply.github.com>
2024-01-21 22:12:55 +0000

    container config

--- .devcontainer/basics/01.js
+++ .devcontainer/basics/01.js
@@ -1,1 +1,11 @@
 console.log("papa")
+const aid = 1213
+let name = "aditya"
+city = "jaipur"
+
+
+console.table([aid,name,city])
+
+console.log(typeof "hitesh");
+console.log(typeof aid);
+console.log(typeof null);
