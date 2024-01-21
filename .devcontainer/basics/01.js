5245df41eaff650d4dc28ee8bd8070535f891be8
adihehe <157320585+adihehe@users.noreply.github.com>
2024-01-21 23:58:27 +0000

    done

--- .devcontainer/basics/01.js
+++ .devcontainer/basics/01.js
@@ -9,3 +9,16 @@ console.table([aid,name,city])
 console.log(typeof "hitesh");
 console.log(typeof aid);
 console.log(typeof null);
+
+// datatypes : String,boolean,number,bignumber,null,symbol
+
+let age = 18
+let stringage = String(18)
+console.log(18);
+console.log(typeof stringage);
+let num = Number("aditya")
+console.log(num);
+console.log(Number(true));
+
+let str1 =
+console.log("1" + 2);
---
+++ .devcontainer/basics/02.js
@@ -0,0 +1,9 @@
+console.log(Number("aditya"));
+console.log("1" + 2);
+console.log(1+2+"3");
+console.log("2" === 2);
+console.log((1 + 2 ** 3 * 4 / 5));
+let age = 2;
+
+console.log(++age);
+console.log(Number(null));
